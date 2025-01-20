2aac0b7332e55e586b8a57ab7980be0c7e52e416
Maxim1 <discordacc.gg@gmail.com>
2025-01-20 12:08:30 +0300

    update w/ animation and shoot pearl print

--- src/enemies.py
+++ src/enemies.py
@@ -45,7 +45,7 @@ class Shell(pygame.sprite.Sprite):
             self.bullet_direction = -1
         else:
             self.frames = frames
-            self.bullet_direction = -1
+            self.bullet_direction = 1
 
         self.frame_index = 0
         self.state = 'idle'
@@ -54,13 +54,37 @@ class Shell(pygame.sprite.Sprite):
         self.old_rect = self.rect.copy()
         self.z = Z_LAYERS['main']
         self.player = player
+        self.shoot_timer = Timer(3000)
+        self.has_fired = False
 
     def state_management(self):
         player_pos, shell_pos = vector(self.player.hitbox_rect.center), vector(self.rect.center)
         player_near = shell_pos.distance_to(player_pos) < 500
+        player_front = shell_pos.x < player_pos.x if self.bullet_direction > 0 else shell_pos.x > player_pos.x
+        player_level = abs(shell_pos.y - player_pos.y) < 30
 
-        if player_near:
-            print('player is near')
+        if player_near and player_front and player_level and not self.shoot_timer.active:
+            self.state = 'fire'
+            self.frame_index = 0
+            self.shoot_timer.activate()
 
     def update(self, dt):
+        self.shoot_timer.update()
         self.state_management()
+
+        #animation
+
+        self.frame_index += ANIMATION_SPEED * dt
+        if self.frame_index < len(self.frames[self.state]):
+            self.image = self.frames[self.state][int(self.frame_index)]
+
+            #fire
+            if self.state == 'fire' and int(self.frame_index) == 3 and not self.has_fired:
+                print('shoot pearl')
+                self.has_fired = True
+
+        else:
+            self.frame_index = 0
+            if self.state == 'fire':
+                self.state = 'idle'
+                self.has_fired = False
